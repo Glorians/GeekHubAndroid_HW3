be1c7a9b686e02279130461ed943fde162d34518
Alex <sallexx@outlook.com>
2019-10-30 17:09:27 +0200

    Don't work

---
+++ app/src/main/java/com/geekhub_android/hw3_beliaev/Adapter.java
@@ -0,0 +1,54 @@
+package com.geekhub_android.hw3_beliaev;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
+
+    private LayoutInflater inflater;
+    private List<Phone> phones;
+
+
+    Adapter(Context context, List<Phone> phones) {
+        this.phones = phones;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(Adapter.ViewHolder holder, int position) {
+        Phone phone = phones.get(position);
+        holder.imageView.setImageResource(phone.getImage());
+        holder.brandView.setText(phone.getBrand());
+        holder.modelView.setText(phone.getModel());
+    }
+
+    @Override
+    public int getItemCount() {
+        return phones.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        final ImageView imageView;
+        final TextView brandView, modelView;
+        ViewHolder(View view){
+            super(view);
+            imageView = (ImageView)view.findViewById(R.id.image);
+            brandView = (TextView) view.findViewById(R.id.brand);
+            modelView = (TextView) view.findViewById(R.id.model);
+        }
+    }
+}
--- app/src/main/java/com/geekhub_android/hw3_beliaev/MainActivity.kt
+++ app/src/main/java/com/geekhub_android/hw3_beliaev/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.geekhub_android.hw3_beliaev
 
+import android.content.ClipData
 import android.os.Bundle
+import android.widget.Adapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_list)
+
+        main_list.layoutManager = LinearLayoutManager(this)
+        main_list.adapter = Adapter()
+
     }
+
+    val phone1 = Phone("Apple", "Iphone X", 1)
+    val phone2 = Phone("Sony", "Xperia 1", 2)
+    val phone3 = Phone("Google", "Pixel 4", 3)
+    val phone4 = Phone("Samsung", "Galaxy Note 10", 4)
+    val phone5 = Phone("OnePlus", "7T", 5)
+    val phone6 = Phone("Asus", "ZenPhone 5", 6)
+    val phone7 = Phone("Nokia", "Lumia 640", 7)
+    val phone8 = Phone("Microsoft", "SurfacePhone", 8)
+
 }
